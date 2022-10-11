31a239f3a10bc979a9505869bf683dbb40e078e6
siranuino <kazu0501ki1995@gmail.com>
2022-10-11 23:41:28 +0900

    ActiveHash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
 
   validates :image,           presence: true
   validates :title,           presence: true
@@ -12,4 +14,5 @@ class Item < ApplicationRecord
   validates :deliverytime_id, presence: true
   validates :price,           presence: true
   validates :user,            presence: true
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_131215) do
+ActiveRecord::Schema.define(version: 2022_10_11_141850) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_131215) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
