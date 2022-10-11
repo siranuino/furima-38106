3ce68317b739e23c44e7ec912f2cf17a17add4d1
siranuino <kazu0501ki1995@gmail.com>
2022-10-11 22:34:47 +0900

    バリデーション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :defrayment_id, :area_id, :deliverytime_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :image,           presence: true
+  validates :title,           presence: true
+  validates :explanation,     presence: true
+  validates :category_id,     presence: true
+  validates :condition_id,    presence: true
+  validates :defrayment_id,   presence: true
+  validates :area_id,         presence: true
+  validates :deliverytime_id, presence: true
+  validates :price,           presence: true
+  validates :user,            presence: true
 end
