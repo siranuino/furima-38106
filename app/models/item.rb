f8c3f578ffcb91c4a43fe8b8db9c5a4c3c77491f
siranuino <kazu0501ki1995@gmail.com>
2022-10-12 22:04:48 +0900

    ActiveHashの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,10 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  belongs_to :item_status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :day
 
   validates :image,           presence: true
   validates :title,           presence: true
@@ -14,5 +18,9 @@ class Item < ApplicationRecord
   validates :deliverytime_id, presence: true
   validates :price,           presence: true
   validates :user,            presence: true
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :genre_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_status_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :charge_id,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id,       numericality: { other_than: 1 , message: "can't be blank"}
 end
