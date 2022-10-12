882515ef238e1978eba0c74e3e21228cab35c1ec
siranuino <kazu0501ki1995@gmail.com>
2022-10-12 23:26:55 +0900

    createアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
