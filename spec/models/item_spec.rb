b46de457b18aeb5939300826b0117d340774b676
siranuino <kazu0501ki1995@gmail.com>
2022-10-14 01:54:39 +0900

    価格のjs表示変更

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -91,6 +91,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+
+      it "priceが全角数字だと登録できない" do
+        @item.price = '１２３４５'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
     end
   end
 end
