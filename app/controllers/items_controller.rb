b1ec6d98abbf08d6cb4a65679aa7edf26baa0268
siranuino <kazu0501ki1995@gmail.com>
2022-10-16 18:34:39 +0900

    ログイン分岐設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.user_id  ==  current_user.id && @item.destroy
+      redirect_to root_path
+      else
+      redirect_to root_path
+    end
   end
 
   private
