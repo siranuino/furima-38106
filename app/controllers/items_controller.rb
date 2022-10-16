20427087504038e693fa5b058e0cd61721ee5c42
siranuino <kazu0501ki1995@gmail.com>
2022-10-16 18:36:11 +0900

    アクション追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :find_item, only: [:show, :edit, :update]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
 
   def index
