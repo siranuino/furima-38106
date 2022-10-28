467749065deb651d7af9d0281657b2744cb2c3ef
siranuino <kazu0501ki1995@gmail.com>
2022-10-28 17:17:57 +0900

    コメント表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,6 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
